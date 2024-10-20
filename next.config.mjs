3e66a8326d0e0df3934373417ac230bff767319a
kishi_masahiro <ksmshr1988@gmail.com>
2024-10-21 00:38:29 +0900

    環境変数修正

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,6 @@ const nextConfig = {
   // 環境変数の設定
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Azureで設定されたAPI URLを使用
-    NODE_ENV: process.env.NODE_ENV, // 環境モード（production）
-    PORT: process.env.PORT || 3000, // ポート番号（デフォルトは3000）
   },
 };
 
